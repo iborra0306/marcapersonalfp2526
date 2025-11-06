abb31039016214f09da2cbd1f57a802b928e4751
Gabriel <2866037@alu.murciaeduca.es>
2025-11-06 11:48:24 +0100

    archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,42 @@ Route::get('/suma/{num1}/{num2}', function($num1, $num2)
     return "La suma de $num1 y $num2 es =  $suma";
 })
 ->where(array('num1' => '[0-9]+', 'num2' => '[0-9]+'));
+
+// Rutas
+Route::get('/', function()
+{
+    return "Pantalla pricnipal";
+});
+
+Route::get('login', function()
+{
+    return "Login usuario";
+});
+Route::get('logout', function()
+{
+    return "Logout usuario";
+});
+
+Route::get('familias-profesionales/show/{id}', function($id)
+{
+    return "Vista detalle proyecto $id";
+})->where('id', '[0-9]+');
+
+Route::get('familias-profesionales/create', function()
+{
+    return "Añadir proyecto";
+})->where('id', '[0-9]+');
+
+Route::get('familias-profesionales/edit/{id}', function($id)
+{
+    return "Modificar proyecto $id";
+})->where('id', '[0-9]+');
+
+Route::get('perfil/{id?}', function($id)
+{
+    if($id){
+        return "Visualizar el perfil de $id";
+    } else {
+        return "Visualizar el perfil propio";
+    }
+})->where('id', '[0-9]+');
