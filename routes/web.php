863d59b1c55bf18602bca0a4403e78c1a42a4d45
Gabriel <2866037@alu.murciaeduca.es>
2026-01-12 09:26:03 +0100

    Breeze API instalado con mantenimiento de la gestión de usuarios

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,22 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'getHome']);
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
 
-// ----------------------------------------
-Route::get('login', function () {
-    return view('auth.login');
-});
-Route::get('logout', function () {
-    return "Logout usuario";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/', [HomeController::class, 'getHome'])
+    ->name('home');
 
 // ----------------------------------------
 Route::prefix('proyectos')->group(function () {
@@ -22,18 +28,14 @@ Route::prefix('proyectos')->group(function () {
 
     Route::get('show/{id}', [ProyectosController::class, 'getShow'])->where('id', '[0-9]+');
 
-    Route::get('edit/{id}', [ProyectosController::class, 'getEdit'])->where('id', '[0-9]+');
-
-    Route::post('store', [ProyectosController::class, 'store']);
-
-    Route::put('update/{id}', [ProyectosController::class, 'update'])->where('id', '[0-9]+');
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('create', [ProyectosController::class, 'getCreate']);
+        Route::post('store', [ProyectosController::class, 'store']);
+        Route::get('edit/{id}', [ProyectosController::class, 'getEdit'])
+            ->where('id', '[0-9]+');
+        Route::put('update/{id}', [ProyectosController::class, 'update'])
+            ->where('id', '[0-9]+');
+    });
 });
 
-
-// ----------------------------------------
-Route::get('perfil/{id?}', function ($id = null) {
-    if ($id === null)
-        return 'Visualizar el currículo propio';
-    return 'Visualizar el currículo de ' . $id;
-}) -> where('id', '[0-9]+');
-
+require __DIR__.'/auth.php';
