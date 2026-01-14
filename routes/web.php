f3263983675c32f7a3e6caf0a83131c6cf27d3cc
Gabriel <2866037@alu.murciaeduca.es>
2026-01-14 09:55:25 +0100

    gabriel

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,22 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'getHome']);
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
 
-// ----------------------------------------
-Route::get('login', function () {
-    return view('auth.login');
-});
-Route::get('logout', function () {
-    return "Logout usuario";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/', [HomeController::class, 'getHome'])
+    ->name('home');
 
 // ----------------------------------------
 Route::prefix('proyectos')->group(function () {
@@ -31,6 +37,22 @@ Route::prefix('proyectos')->group(function () {
         Route::put('update/{id}', [ProyectosController::class, 'update'])
             ->where('id', '[0-9]+');
     });
+});
+
+Route::prefix('familias-profesionales')->group(function () {
+    Route::get('/', [FamiliasProfesionalesController::class, 'getIndex']);
+
+    Route::get('create', [FamiliasProfesionalesController::class, 'getCreate']);
+
+    Route::get('/show/{id}', [FamiliasProfesionalesController::class, 'getShow']) -> where('id', '[0-9]+');
+
+    Route::get('/edit/{id}', [FamiliasProfesionalesController::class, 'getEdit']) -> where('id', '[0-9]+');
+
+    Route::post('store', [FamiliasProfesionalesController::class, 'store']);
+
+    Route::put('update/{id}', [FamiliasProfesionalesController::class, 'update'])-> where('id', '[0-9]+');
+
+
 });
 
 require __DIR__.'/auth.php';
